a10ec0461019bb75e6ad3b65e20224763c774035
Nguyen <Truc.Nguyen@jacobs.com>
2022-11-17 11:27:49 -0500

    initial commit for sam project

---
+++ hello_world/app.py
@@ -0,0 +1,13 @@
+import json
+
+def lambda_handler(event, context):
+    first_name = event['first_name']
+    last_name = event['last_name']
+    message = event['message']
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": f"{last_name}, {first_name} {message}",
+        }),
+    }
