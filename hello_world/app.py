274a84780f6458cc6383459bfcb98d4669ef703f
Nguyen <Truc.Nguyen@jacobs.com>
2022-11-18 15:11:08 -0500

    update text

--- hello_world/app.py
+++ hello_world/app.py
@@ -8,6 +8,6 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": f"{last_name}, {first_name} {message}",
+            "message": f"{last_name}, {first_name} {message} update",
         }),
     }
